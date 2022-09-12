ebae308bfaf397dba875d862e75ad60ac7631cf6
CodeRider000 <112244064+CodeRider000@users.noreply.github.com>
2022-09-12 22:59:04 +0530

    Update Invoice.java
    added few new comments and added scanner

--- Invoice.java
+++ Invoice.java
@@ -1,69 +1,69 @@
+import java.util.Scanner;
 // this is a program to imitate the invoice of a store which has instances like item code , name , quantity,
 // price per item
 
 // a class called invoice is created
 class Invoice{
-    String itemCode , name;
-    int quantity;
-    double pricePerItem;
- 
-    Invoice(String ic , String n , int q , double ppi){
-        itemCode = ic;
-        name = n;
-        quantity = q;
-        pricePerItem = ppi;
-    }
+    private String itemCode , name;
+    private int quantity;
+    private double pricePerItem;
 
+    Invoice(){
+        itemCode = "";
+        name = "";
+        quantity = 0;
+        pricePerItem = 0.0;
+    }
     void setItemCode(String newCode){
-        itemCode = newCode;
+        this.itemCode = newCode;
     }
     // a method called setItemCode is created , it is used to set a new item code
     void setName(String newName){
-        name = newName;
+        this.name = newName;
     }
     // a method called setName is created , it is used to set a new name
     void setQuantity(int newQuantity){
         if(newQuantity < 0){
-            quantity = 0;
+            this.quantity = 0;
         }
         else{
-            quantity = newQuantity;
+            this.quantity = newQuantity;
         }
     }
         // a method called setQuantity is created , it is used to set a new no of quantity
 
     void setPricePerItem(double newPrice){
         if (newPrice<0){
-            pricePerItem = 0.0;
+            this.pricePerItem = 0.0;
         }
         else{
-            pricePerItem = newPrice;
+            this.pricePerItem = newPrice;
         }
     }
         // a method called setPricePerItem is created , it is used to set a new price for item
 
     String getItemCode(){
-        return itemCode;
+        return this.itemCode;
     }
         // a method called getItemCode is created , it returns itemcode
 
     String getName(){
-        return name;
+        return this.name;
     }
             // a method called getName is created , it returns name
 
     int getQuantity(){
-        return quantity;
+        return this.quantity;
     }
             // a method called getQuantity is created , it returns qunatity
 
     double getPricePerItem(){
-        return pricePerItem;
+        return this.pricePerItem;
     }
             // a method called getPricePerItem is created , it returns price per item
 
     double getInvoiceAmount(){
-        return quantity*pricePerItem;
+        return this.quantity*this.pricePerItem;
     }
     // this method return the total amount from the given quantity and price per item
 
@@ -71,19 +71,26 @@ class Invoice{
 }
 class Main{
     public static void main(String[] args) {
+        // creating a scanner object to scan input from user
+        Scanner scan = new Scanner(System.in);
         // creating a new Invoice object
-        Invoice ob = new Invoice("1234","pen",20,3.75);
-        // saving the total 
-        double total = ob.getInvoiceAmount();
-        System.out.println("total price = "+total);
-        ob.setItemCode("1942321");
-        ob.setName("water bottle");
-        ob.setPricePerItem(99);
-        ob.setQuantity(21);
-        total = ob.getInvoiceAmount();
-        System.out.println("item :"+ob.getName()+"code : "+ob.getItemCode()+" quantity : "
-                                                +ob.getQuantity()+" Price(per item) : "+ob.getPricePerItem()+" Total price : "+ob.getInvoiceAmount());
-    
+        Invoice ob = new Invoice();
+        //getting input form user
+        System.out.println("Enter Item Name :");
+        String itemName = scan.nextLine();
+        System.out.println("Enter Item Code :");
+        String itemCode = scan.nextLine();
+        System.out.println("Enter Quantity :");
+        int q = scan.nextInt();
+        System.out.println("Enter Price(per item)");
+        double ppi = scan.nextDouble();
+        //set the input data from user to instances
+        ob.setItemCode(itemCode);
+        ob.setName(itemName);
+        ob.setPricePerItem(ppi);
+        ob.setQuantity(q);
+        //printing the final verdict
+        System.out.println("item :"+ob.getName()+"\ncode : "+ob.getItemCode()+"\nquantity : "
+                                                +ob.getQuantity()+"\nPrice(per item) : "+ob.getPricePerItem()+"\nTotal price : "+ob.getInvoiceAmount());   
 }
-
 }
